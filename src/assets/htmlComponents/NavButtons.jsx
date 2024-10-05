97f7abcd5214cb21bd289fbcc7353b86af4693cb
Henry <tongsik98@gmail.com>
2024-10-04 21:53:14 -0700

    added cover letter

--- src/assets/htmlComponents/NavButtons.jsx
+++ src/assets/htmlComponents/NavButtons.jsx
@@ -9,28 +9,28 @@ export default function NavButtons({
     <>
       <div className="absolute top-0 right-0 flex justify-center z-20">
         <button
-          className="m-2.5 font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
+          className="h-12 m-2.5 font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
           style={{ color: lightOn ? "black" : "white", width: "110px" }}
           onClick={aboutClick}
         >
           About me
         </button>
         <button
-          className="m-2.5 font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
+          className="h-12 m-2.5 font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
           style={{ color: lightOn ? "black" : "white", width: "110px" }}
           onClick={projectsClick}
         >
           Projects
         </button>
         <button
-          className="m-2.5 font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
+          className="h-12 m-2.5 font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
           style={{ color: lightOn ? "black" : "white", width: "110px" }}
           onClick={skillsClick}
         >
           Skills
         </button>
         <button
-          className="m-2.5 font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
+          className="h-12 m-2.5 font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
           style={{ color: lightOn ? "black" : "white", width: "110px" }}
           onClick={contactClick}
         >
@@ -43,12 +43,26 @@ export default function NavButtons({
           rel="noopener noreferrer"
         >
           <button
-            className="m-2.5 font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
+            className="h-12 m-2.5 font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
             style={{ color: lightOn ? "black" : "white", width: "110px" }}
           >
             Resume
           </button>
         </a>
+        <a
+          href="/Henry_Chung_CoverLetter_GameDev.pdf"
+          className="hover:underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button 
+            className="h-12 m-2.5 text-sm font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
+            style={{ color: lightOn ? "black" : "white", width: "110px" }}
+          >
+            Cover Letter
+          </button>
+        </a>
+
         <a
           href="https://henrychung98.github.io/"
           className="hover:underline"
@@ -56,7 +70,7 @@ export default function NavButtons({
           rel="noopener noreferrer"
         >
           <button
-            className="m-2.5 font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
+            className="h-12 m-2.5 font-bold border-2 border-blue-500 rounded p-2 bg-gray-200 bg-opacity-50 hover:opacity-50 duration-300"
             style={{ color: lightOn ? "black" : "white", width: "110px" }}
           >
             Blog
